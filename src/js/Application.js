3bf7ad56c2be03bf92a5a963f6294f2d7484943b
Jordan <jordanasenovzhelyazkov@gmail.com>
2021-09-19 20:02:13 +0300

    fixed Application class and Beat class

--- src/js/Application.js
+++ src/js/Application.js
@@ -7,19 +7,35 @@ export default class Application extends EventEmitter {
       READY: "ready",
     };
   }
+  _beat = new Beat();
+  
 
   constructor() {
     super();
+    
 
-    const lyrics = ["Ah", "ha", "ha", "ha", "stayin' alive", "stayin' alive"];
-    let count = 0;
-
-    const message = document.createElement("div");
-    message.classList.add("message");
-    message.innerText = "Ah";
-
-    document.querySelector(".main").appendChild(message);
-
+    const interval = setInterval(() => {
+      const lyrics = ["Ah", "ha", "ha", "ha", "stayin' alive", "stayin' alive"];
+      let count = 0;
+      for (const lyric of lyrics) {
+        if(count > lyric.length){
+          clearInterval(interval)
+        }
+        const message = document.createElement("div");
+      message.classList.add("message");
+      message.innerText = lyric;
+  
+      document.querySelector(".main").appendChild(message);
+      count++
+      }
+    }, 600);
+  
+    
     this.emit(Application.events.READY);
+  }
+  
+
+  _create(){
+    
   }
 }
